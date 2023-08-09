940a7348805b54b18d35ca884dd24d9dad0b31fe
evans86 <evans86@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>
2023-08-09 19:47:31 +0300

    add all log

--- app/Http/Controllers/Activate/BotController.php
+++ app/Http/Controllers/Activate/BotController.php
@@ -13,7 +13,7 @@ class BotController
     {
         $bots = Bot::orderBy('id', 'DESC')->Paginate(10);
 
-        $newBots = count(Bot::query()->where('created_at', '>', '2023-07-10 21:13:25')->get());
+        $newBots = count(Bot::query()->where('created_at', '>', '2023-08-09 18:13:25')->get());
         $allCount = count(Bot::get());
 
         return view('activate.bot.index', compact(
