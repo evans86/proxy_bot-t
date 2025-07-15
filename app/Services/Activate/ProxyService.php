c92105ba96b68952911f8c9f57d429f995ddb1d2
evans <evandaniil85@gmail.com>
2025-07-15 20:14:30 +0300

    fix module balance

--- app/Services/Activate/ProxyService.php
+++ app/Services/Activate/ProxyService.php
@@ -395,16 +395,17 @@ class ProxyService extends MainService
     {
         $proxyApi = new ProxyApi($botDto->api_key);
 
-//        $proxies = \Cache::get('proxies');
-//        if($proxies === null){
+        $proxies = \Cache::get('proxies');
+        if($proxies === null){
             $proxies = Proxy::all();
-//            \Cache::put('proxies', $proxies, 900);
-//        }
+            \Cache::put('proxies', $proxies, 900);
+        }
 
         $result = [];
         foreach ($proxies as $key => $proxy) {
 
             $countries = $proxyApi->getcountry($proxy->version);
+            BotLogHelpers::notifyBotLog('(🔵E ' . __FUNCTION__ . ' Proxy): ' . $countries);
             $countries = $countries['list'];
 
             $countriesArr = [];
